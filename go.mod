670e87ee384728ed9ab8adba837555d804fd3fe2
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-03-23 15:11:03 -0300

    token types

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Luisgustavom1/regex-engine
+
+go 1.21.4
---
+++ parser.go
@@ -0,0 +1,22 @@
+package main
+
+type tokenType uint8
+
+const (
+	group           tokenType = iota
+	bracket         tokenType = iota
+	or              tokenType = iota
+	repeat          tokenType = iota
+	literal         tokenType = iota
+	groupUncaptured tokenType = iota
+)
+
+type token struct {
+	tokenType tokenType
+	value     interface{}
+}
+
+type parseContext struct {
+	pos    int
+	tokens []token
+}
